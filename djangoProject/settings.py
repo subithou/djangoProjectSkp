4ed8ff5a8e80d09587e748a8683455052c973aae
Subith O U <ousubith@gmail.com>
2022-03-09 07:24:44 +0530

    commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) # new
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) # new
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
