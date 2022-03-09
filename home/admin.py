b82292dcc6c9b6e42f3f8b97df5e1bfbce635e08
Subith O U <ousubith@gmail.com>
2022-03-09 08:10:52 +0530

    commit

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from home.models import profile, team
+
 # Register your models here.
+@admin.register(profile)
+class profileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(team)
+class teamAdmin(admin.ModelAdmin):
+    pass
