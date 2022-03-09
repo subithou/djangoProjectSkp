11a3df1bdf5f439fdf38e225f435bd80fd6a44b1
Subith O U <ousubith@gmail.com>
2022-03-09 06:24:47 +0530

    commit

--- home/models.py
+++ home/models.py
@@ -4,8 +4,13 @@ from django.db import models
 class profile(models.Model):
     name = models.CharField(max_length=255, null=False)
     profile_photo = models.ImageField(upload_to='images/', null=True, default='images/user_default_image.png')
+    play = models.CharField(max_length=255, null=False, default='Boller')
     team =models.IntegerField(null=True, default=None)
     amount = models.PositiveBigIntegerField(default=None, null=True)
+    
+    def __str__(self):
+        name = self.name
+        return name
 
 class team(models.Model):
     team_name = models.CharField(max_length=255, null=False)
@@ -13,3 +18,7 @@ class team(models.Model):
     team_owner = models.CharField(max_length=255, null=False)
     original_amount = models.PositiveBigIntegerField(default=None, null=True)
     balance_amount = models.PositiveBigIntegerField(default=None, null=True)
+
+    def __str__(self):
+        name = self.team_name
+        return name
