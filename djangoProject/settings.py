5506b7f485ecac57c86aad7f9ff09c12a26ab640
Subith O U <ousubith@gmail.com>
2022-03-10 00:24:06 +0530

    commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Password validation
