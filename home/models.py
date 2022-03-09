7b5caa89c7f44d70fd5ed7dc8430193dafdb9a45
Subith O U <ousubith@gmail.com>
2022-03-09 21:28:29 +0530

    Merge branch 'master' of https://github.com/subithou/djangoProject_auction

--- home/models.py
+++ home/models.py
@@ -1,24 +1,24 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
 class profile(models.Model):
-    name = models.CharField(max_length=255, null=False)
-    profile_photo = models.ImageField(upload_to='images/', null=True, default='images/user_default_image.png')
-    play = models.CharField(max_length=255, null=False, default='Boller')
-    team =models.IntegerField(null=True, default=None)
+    player_id = models.CharField(max_length=255, null=False, unique=True)
+    profile_photo = models.ImageField(upload_to='images/', null=True, default='images/user_default_image.png')  
+    team =models.CharField(max_length=255, null=True, default=None)
     amount = models.PositiveBigIntegerField(default=None, null=True)
     
     def __str__(self):
-        name = self.name
+        name = self.player_id
         return name
 
 class team(models.Model):
-    name = models.CharField(max_length=255, null=False)
+    name = models.CharField(max_length=255, null=False, unique=True)
     manager = models.CharField(max_length=255, null=False)
     owner = models.CharField(max_length=255, null=False)
     original_amount = models.PositiveBigIntegerField(default=None, null=True)
     balance_amount = models.PositiveBigIntegerField(default=None, null=True)
 
     def __str__(self):
-        name = self.team_name
+        name = self.name
         return name
