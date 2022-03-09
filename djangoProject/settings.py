4e962fc8b0140f1024e176bb4b19fcd5f401d11f
Subith O U <ousubith@gmail.com>
2022-03-10 00:25:46 +0530

    commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
     }
 }
 
-#WHITENOISE_USE_FINDERS = True
+WHITENOISE_USE_FINDERS = True
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
